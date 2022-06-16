6ef65146edde2e62c5ac4dea4ae66f87e20d71f4
s175573 <44926987+s175573@users.noreply.github.com>
2022-06-15 21:06:59 -0500

    Update LungCancer_KNNpred.R

--- LungCancer_KNNpred.R
+++ LungCancer_KNNpred.R
@@ -61,3 +61,68 @@ knn.pred(sqrt(1-tmp.cor), tmp.cols, n=3) -> cMat
 tmp.rr=cMat[,1]/cMat[,2]
 tmp.yy=2-tmp.col.test2
 plot(roc(tmp.yy ~ tmp.rr))  ## AUC=0.97
+
+## build pvalTables
+tmp.dd=cbind(RFU.covid.adaptive, RFU.lung.MDA, RFU.melanoma.riaz, RFU.shipp.baseline)
+tmp.lab=c(rep('COVID-19',160), rep('LDA',121),rep('Melanoma',58),rep('Lymphoma',56))
+pvalTable = c()
+for(cc in c('COVID-19','LDA','Melanoma','Lymphoma')){
+  vvc=which(tmp.lab==cc)
+  vvn=which(tmp.lab!=cc)
+  pvalList = c()
+  fcList = c()
+  for(ii in 1:5000){
+    tmp=t.test(tmp.dd[ii,vvc], tmp.dd[ii,vvn])
+    tmp.fc = mean(tmp.dd[ii,vvc])/mean(tmp.dd[ii,vvn])
+    pvalList = c(pvalList, tmp$p.value)
+    fcList = c(fcList, tmp.fc)
+  }
+  pvalList.a = p.adjust(pvalList, 'BH')
+  dd1=data.frame(RFU=1:5000, Disease=cc, FC=fcList, AdjustP=pvalList.a)
+  pvalTable = rbind(pvalTable, dd1)
+}
+
+pvalTable0 = pvalTable
+
+tmp.dd=cbind(RFU.covid.adaptive, RFU.lung.cascone, RFU.melanoma.riaz, RFU.shipp.baseline)
+tmp.lab=c(rep('COVID-19',160), rep('LDA',53),rep('Melanoma',58),rep('Lymphoma',56))
+pvalTable = c()
+for(cc in c('COVID-19','LDA','Melanoma','Lymphoma')){
+  vvc=which(tmp.lab==cc)
+  vvn=which(tmp.lab!=cc)
+  pvalList = c()
+  fcList = c()
+  for(ii in 1:5000){
+    tmp=t.test(tmp.dd[ii,vvc], tmp.dd[ii,vvn])
+    tmp.fc = mean(tmp.dd[ii,vvc])/mean(tmp.dd[ii,vvn])
+    pvalList = c(pvalList, tmp$p.value)
+    fcList = c(fcList, tmp.fc)
+  }
+  pvalList.a = p.adjust(pvalList, 'BH')
+  dd1=data.frame(RFU=1:5000, Disease=cc, FC=fcList, AdjustP=pvalList.a)
+  pvalTable = rbind(pvalTable, dd1)
+}
+
+
+
+## Use pvalTable to find predictive RFUs for Serum data
+pvalTable0[which(pvalTable0[,2]=='LDA' & pvalTable0[,3]>=1.1 & pvalTable0[,4]<= 0.05),1] -> tmp.rr1## using RFU.lung.MDA
+pvalTable[which(pvalTable[,2]=='LDA' & pvalTable[,3]>=1.1 & pvalTable[,4]<= 0.05),1] -> tmp.rr2 ## using RFU.lung cascone
+intersect(tmp.rr1, tmp.rr2)  ## 1472
+pvalTable0[which(pvalTable0[,2]=='COVID-19' & pvalTable0[,4]<= 1e-11),]
+pvalTable0[which(pvalTable0[,2]=='LDA' & pvalTable0[,3]>1.2 & pvalTable0[,4]<=1e-4),]  ## included 4033
+boxplot(RFU.yilong[1472,tmp.ss] - RFU.yilong[2799,tmp.ss] ~ serum.info[tmp.ss,'is_cancer'])
+
+boxplot(RFU.yilong[1472,tmp.ss] + RFU.yilong[4033,tmp.ss] - 2*RFU.yilong[2799,tmp.ss] ~ serum.info[tmp.ss,'is_cancer'])
+
+tmp.xx = RFU.yilong[1472,tmp.ss]  - 2*RFU.yilong[2799,tmp.ss]
+tmp.yy = as.numeric(as.factor(serum.info[tmp.ss,'is_cancer']))
+plot(roc(tmp.yy ~ tmp.xx), lwd=3, cex.lab=1.5, cex.axis=1.5)
+
+tmp.rot = prcomp(t(RFU.covid.adaptive))$rotation
+tmp.pca1 = t(RFU.yilong) %*% tmp.rot
+
+boxplot(RFU.yilong[1472,tmp.ss] ~ serum.info[tmp.ss,'is_cancer'], xlab='',ylab='RFU 4033', cex.lab=1.5, cex.axis=1.5, lwd=3, col='orange')
+
+tmp.xx = RFU.yilong[1472,tmp.ss] + RFU.yilong[4033,tmp.ss] - 2*RFU.yilong[2799,tmp.ss] + tmp.pca1[tmp.ss,3]
+boxplot(tmp.xx ~ serum.info[tmp.ss,'is_cancer'], xlab='',ylab='RFU score', cex.lab=1.5, cex.axis=1.5, lwd=3, col='orange')
